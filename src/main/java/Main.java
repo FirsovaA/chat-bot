47dc7ab76e4c9098aac09f8d26d5d7a8e5b11523
FirsovaA <firsovasasha123@gmail.com>
2019-09-25 00:53:00 +0430

    сохранение шуток в избранное
    сохранение шуток, сохранение истории пользователя

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
+package main.java;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
         ChatBot chatBot = new ChatBot();
-        System.out.println(chatBot.reply("/help"));
+        System.out.println(chatBot.reply(""));
 
         while (!chatBot.over){
             String input = scanner.nextLine();
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,43 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//TODO User tests
+class UserTest {
+
+    @Test
+    void createNewUser() {
+
+    }
+
+    @Test
+    void loadUserFromFile(){
+
+    }
+
+    @Test
+    void saveJokes() {
+
+    }
+
+    @Test
+    void saveRepetitiveJokes() {
+
+    }
+
+    @Test
+    void saveMoreThanAvailableJokes() {
+
+    }
+
+    @Test
+    void saveNewUserData() {
+    }
+
+    @Test
+    void saveExistingUserData() {
+
+    }
+}
