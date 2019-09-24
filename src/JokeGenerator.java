e603da8482cde03ec310ac6eae608a9d001a6dfd
K413K4 <46976623+K413K4@users.noreply.github.com>
2019-09-24 21:34:50 +0500

    Сделал генератор анекдотов

--- src/JokeGenerator.java
+++ src/JokeGenerator.java
@@ -1,5 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.google.gson.*;
+
 public class JokeGenerator {
-    public static String getJoke(){
-        return "That joke about the hat";
+    class Joke{
+        public String content;
+    }
+    public  static String getJoke() {
+        String query = "http://rzhunemogu.ru/RandJSON.aspx?CType=1";
+        HttpURLConnection connection = null;
+        String out = "no joke todey";
+        try {
+            connection = (HttpURLConnection) new URL(query).openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setUseCaches(false);
+            connection.setConnectTimeout(250);
+            connection.setReadTimeout(250);
+
+            connection.connect();
+
+            StringBuilder sb = new StringBuilder();
+
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
+                BufferedReader anekdot = new BufferedReader(new InputStreamReader(connection.getInputStream(), "cp1251"));
+
+                String line;
+                while ((line = anekdot.readLine()) != null) {
+                    sb.append(line);
+                    sb.append("\n");
+                }
+
+                //System.out.println(sb.toString());
+                Joke joke = new Gson().fromJson(sb.toString(), Joke.class);
+                out = joke.content;
+            } else {
+                out = "fail: " + connection.getResponseCode() + ", " + connection.getResponseMessage();
+            }
+        } catch(Throwable cause){
+            //cause.printStackTrace();
+        } finally{
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return out;
     }
 }
